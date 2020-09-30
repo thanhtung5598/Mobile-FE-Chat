1dc8af565de662d9b79e615e801232beb32117ce
MacBook <macbook@MacBooks-MacBook-Pro.local>
2020-09-30 18:44:51 +0700

    RM

--- .prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  tabWidth: 2,
-  arrowParens: "avoid",
-  trailingComma: "none"
-};
--- src/redux/store/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import appReducer from '../reducer';
-
-const store = createStore(appReducer);
-
-export default store;
