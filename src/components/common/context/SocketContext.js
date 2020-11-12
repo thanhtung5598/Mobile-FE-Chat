b541b976fed611ea893ab1f7a7a96893e981ccbb
LeThanhTung <thanhtung5598@gmail.com>
2020-11-12 23:50:21 +0700

    Update Socket

--- src/components/common/context/SocketContext.js
+++ src/components/common/context/SocketContext.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import useSocket from 'use-socket.io-client';
+import { useSelector } from 'react-redux';
 
 const SocketContext = React.createContext();
 const SocketConsumer = SocketContext.Consumer;
 
 const SocketProvider = props => {
+  const { auth_token } = useSelector(state => state.authen);
+  const ENDPOINT = `https://api-chat.ga?token=${auth_token}`;
+  const [socket] = useSocket(ENDPOINT, {
+    serveClient: false,
+    // below are engine.IO options
+    origins: '*:*',
+    transports: ['polling'],
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
+  });
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log(socket.connected);
+    });
+  }, [socket]);
   return (
     <SocketContext.Provider value={{}}>{props.children}</SocketContext.Provider>
   );
