c07e48a49026cd61b64b028d19ae1d7872b2cf36
LeThanhTung <thanhtung5598@gmail.com>
2020-11-13 00:15:07 +0700

    Chat

--- src/components/common/context/SocketContext.js
+++ src/components/common/context/SocketContext.js
@@ -7,6 +7,7 @@ const SocketContext = React.createContext();
 const SocketConsumer = SocketContext.Consumer;
 
 const SocketProvider = props => {
+  // const [room, setRoom] = useState(null);
   const { auth_token } = useSelector(state => state.authen);
   const ENDPOINT = `https://api-chat.ga?token=${auth_token}`;
   const [socket] = useSocket(ENDPOINT, {
@@ -19,8 +20,31 @@ const SocketProvider = props => {
     cookie: false
   });
   useEffect(() => {
-    socket.on('connect', () => {
-      console.log(socket.connected);
+    const info = {
+      // them list_user > 2 se tao gr
+      list_user: [
+        {
+          name: 'Đoan Trang',
+          id: 11
+        },
+        {
+          name: 'Minh Thư',
+          id: 12
+        }
+      ],
+      // vi group la phai tao truoc nen da co name, nen trong day ko bo name room neu tao moi
+      // chi can doi roomId la se co message khong can doi list_user neu khong phai tao moi
+      roomId: '5fabeb92529224006d6b1db4', // if roomId khong ton tai se tu tao 1 room moi, if 2 user da co room single thi se tu dong tao group co 2 ng
+      // con voi group bat buoc phai co id truoc
+      // roomId rong hoac khong dung dinh dang se ko hien thi
+      positionUserCurrent: 1 // day la vi tri user dang login laf Truong
+    };
+    //Khi người dùng click vào 1 cuộc trò chuyện nào đó thì gọi cái này và truyền dữ liệu vào (truyen user va user tro chuyen cùng)
+    socket.emit('join', info);
+    socket.on('load_message', function (user_id, msg) {
+      if (user_id === info.list_user[info.positionUserCurrent].id) {
+        console.log(msg);
+      }
     });
   }, [socket]);
   return (
